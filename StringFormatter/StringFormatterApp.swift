0e3226d7ceed974ad6360435c5d649f0aa0e017d
Zachary Burk <zachburk7@gmail.com>
2023-09-23 13:11:03 -0400

    Initial Commit

---
+++ StringFormatter/StringFormatterApp.swift
@@ -0,0 +1,17 @@
+//
+//  StringFormatterApp.swift
+//  StringFormatter
+//
+//  Created by Zachary Burk on 9/23/23.
+//
+
+import SwiftUI
+
+@main
+struct StringFormatterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

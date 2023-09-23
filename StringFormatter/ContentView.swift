ca827acf7f446d87f326f82c7a01d56069028790
Zachary Burk <zachburk7@gmail.com>
2023-09-23 14:49:17 -0400

    Initial commit

--- StringFormatter/ContentView.swift
+++ StringFormatter/ContentView.swift
@@ -7,15 +7,80 @@
 
 import SwiftUI
 
+struct ConvertCaseOption {
+    var title: String
+    var conversion: (_ text: String) -> String
+}
+
+func uppercaseConversion (_ text: String) -> String {
+    return text.capitalized
+}
+
 struct ContentView: View {
+    var allConversionOptions: [ConvertCaseOption] = [
+        ConvertCaseOption(title: "Plain Text", conversion: { (_ text: String) -> String in
+            return NSAttributedString(string: text).string
+        }),
+        ConvertCaseOption(title: "Sentence case", conversion: { (_ text: String) -> String in
+            let sentenceCase = text.prefix(1).capitalized + text.dropFirst().lowercased()
+            return sentenceCase
+        }),
+        ConvertCaseOption(title: "lower case", conversion: { (_ text: String) -> String in
+            return text.lowercased()
+        }),
+        ConvertCaseOption(title: "UPPER CASE", conversion: { (_ text: String) -> String in
+            return text.uppercased()
+        }),
+        ConvertCaseOption(title: "Capitalized Case", conversion: { (_ text: String) -> String in
+            return text.capitalized(with: .autoupdatingCurrent)
+        }),
+        ConvertCaseOption(title: "AlTeRnAtInG cAsE", conversion: { (_ text: String) -> String in
+            var result = ""
+            var shouldCapitalizeNext = true
+            
+            for character in text {
+                if character == " " {
+                    result.append(character)
+                } else {
+                    if shouldCapitalizeNext {
+                        result.append(character.uppercased())
+                    } else {
+                        result.append(character.lowercased())
+                    }
+                    
+                    shouldCapitalizeNext.toggle()
+                }
+            }
+
+            return result
+        })
+    ]
+
+    func readClipboard() -> String {
+        return NSPasteboard.general.string(forType: .string) ?? ""
+    }
+    
+    func copyToClipboard(_ text: String) {
+        let pasteboard = NSPasteboard.general
+        pasteboard.clearContents()
+        pasteboard.setString(text, forType: .string)
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Group {
+            ForEach(Array(allConversionOptions.enumerated()), id: \.element.title) { index, conversionOption in
+                Button(conversionOption.title) {
+                    copyToClipboard(conversionOption.conversion(readClipboard()))
+                }
+                .keyboardShortcut(KeyboardShortcut(KeyEquivalent(Character("\(index + 1)"))))
+            }
+            
+            Divider()
+            
+            Button("Quit") {
+                NSApplication.shared.terminate(nil)
+            }.keyboardShortcut("q")
         }
-        .padding()
     }
 }
 
--- StringFormatter/StringFormatterApp.swift
+++ StringFormatter/StringFormatterApp.swift
@@ -10,7 +10,7 @@ import SwiftUI
 @main
 struct StringFormatterApp: App {
     var body: some Scene {
-        WindowGroup {
+        MenuBarExtra("String Modifier", systemImage: "pencil.circle") {
             ContentView()
         }
     }
